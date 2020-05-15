b7b47e453ecaa946408b31e9b5658528683ee312
Vincent Borden <vinbor81@gmail.com>
2020-05-15 19:37:48 -0400

    Stub Boot/MVC project

---
+++ RestaurantApp/src/test/java/com/skilldistillery/restaurants/RestaurantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.restaurants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
